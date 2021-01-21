260ba07a3dad172293871a974595e573b6a98baa
maiaij <maia.johnson4@gmail.com>
2021-01-21 00:21:42 -0500

    made print function

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -1,6 +1,10 @@
 package lab01;
 public class lab01{
     public static void main(String[] args){
+        print();
+    }
+
+    public static void print(){
         System.out.println("why");
     }
 }
